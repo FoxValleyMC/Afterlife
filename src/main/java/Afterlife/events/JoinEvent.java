45be2df14267af05d920aba0904933f5f8c2a027
Steve Coulter <stevecoulter99@gmail.com>
2020-02-06 11:33:02 -0600

    ftps

--- src/main/java/Afterlife/events/JoinEvent.java
+++ src/main/java/Afterlife/events/JoinEvent.java
@@ -1,48 +1,60 @@
 package Afterlife.events;
 
+import Afterlife.Main;
+import Afterlife.handler.FloatingTextHandler;
+import NukkitDB.NukkitDB;
+import PlayerAPI.Overrides.PlayerAPI;
+import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.level.Level;
+import cn.nukkit.level.Position;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class JoinEvent implements Listener {
 
-//    private Main plugin;
-//
-//    public JoinEvent(Main plugin) {
-//        this.plugin = plugin;
-//    }
-//
-//    @EventHandler(priority = EventPriority.NORMAL)
-//    public void onJoin(PlayerJoinEvent event) {
-//        PlayerAPI player = (PlayerAPI) event.getPlayer();
-//        String uuid = player.getUniqueId().toString();
-//
-//        if (Mongodb.query(uuid, "uuid") == null) {
-//            Map<String, Object> objectMap = new HashMap<>();
-//            objectMap.put("uuid", uuid);
-//            objectMap.put("name", player.getName().toLowerCase());
-//            objectMap.put("kills", 0);
-//            objectMap.put("kill-streak", 0);
-//            objectMap.put("deaths", 0);
-//            objectMap.put("experience", 0);
-//            objectMap.put("global-xp", 0);
-//            objectMap.put("levels", 0);
-//            Mongodb.createNew(objectMap);
-//        }
-//
-//        // render leaderboards
-//        Map<String, Object> objectMap = plugin.texts.getAll();
-//        if (!objectMap.isEmpty()) {
-//            for (String key : objectMap.keySet()){
-//                Map<String, Object> dataMap = (Map<String, Object>) plugin.texts.get(key);
-//                Level level = plugin.getServer().getLevelByName(dataMap.get("level").toString());
-//                double x = (double) dataMap.get("x");
-//                double y = (double) dataMap.get("y");
-//                double z = (double) dataMap.get("z");
-//                Position position = new Position(x, y+1.7, z, level);
-//                FloatingTextHandler.addParticle(position, key, player);
-//            }
-//        }
-//    }
+    private Main plugin;
+
+    public JoinEvent(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler()
+    public void onJoin(PlayerJoinEvent event) {
+        PlayerAPI player = (PlayerAPI) event.getPlayer();
+        String uuid = player.getUuid();
+
+        String database = plugin.getConfig().getString("database");
+        String collection = plugin.getConfig().getString("collection");
+
+        if (NukkitDB.query(uuid, "uuid", database, collection) == null) {
+            Map<String, Object> objectMap = new HashMap<>();
+            objectMap.put("uuid", uuid);
+            objectMap.put("name", player.getName().toLowerCase());
+            objectMap.put("kills", 0);
+            objectMap.put("kill-streak", 0);
+            objectMap.put("deaths", 0);
+            objectMap.put("experience", 0);
+            objectMap.put("global-xp", 0);
+            objectMap.put("levels", 0);
+            NukkitDB.insertDocument(objectMap, database, collection);
+        }
+
+
+        // render leaderboards
+        Map<String, Object> objectMap = plugin.texts.getAll();
+        if (!objectMap.isEmpty()) {
+            for (String key : objectMap.keySet()){
+                Map<String, Object> dataMap = (Map<String, Object>) plugin.texts.get(key);
+                Level level = plugin.getServer().getLevelByName(dataMap.get("level").toString());
+                double x = (double) dataMap.get("x");
+                double y = (double) dataMap.get("y");
+                double z = (double) dataMap.get("z");
+                Position position = new Position(x, y+1.7, z, level);
+                FloatingTextHandler.addParticle(position, key, player);
+            }
+        }
+    }
 }
--- src/main/java/Afterlife/handler/FloatingTextHandler.java
+++ src/main/java/Afterlife/handler/FloatingTextHandler.java
@@ -1,13 +1,22 @@
 package Afterlife.handler;
 
+import Afterlife.Main;
+import Afterlife.modules.GetData;
+import Afterlife.utils.Colorize;
+import PlayerAPI.Overrides.PlayerAPI;
+import cn.nukkit.level.particle.FloatingTextParticle;
+import cn.nukkit.math.Vector3;
+
+import java.util.Map;
+
 public class FloatingTextHandler {
 
-//    public static void addParticle(Vector3 location, String type, Player player) {
-//        Map<String, Object> objectMap = (Map<String, Object>) Main.getPlugin().getConfig().get("texts-title");
-//        String title = objectMap.get(type).toString();
-//        FloatingTextParticle particle = new FloatingTextParticle(location, Colorize.Register(title)+"\n"+GetData.GetLeaderboard(type));
-//        player.getLevel().addParticle(particle);
-//        Main.getPlugin().ftps.put(type, particle);
-//    }
+    public static void addParticle(Vector3 location, String type, PlayerAPI player) {
+        Map<String, Object> objectMap = (Map<String, Object>) Main.getInstance().getConfig().get("texts-title");
+        String title = objectMap.get(type).toString();
+        FloatingTextParticle particle = new FloatingTextParticle(location, Colorize.Register(title)+"\n"+ GetData.GetLeaderboard(type));
+        player.getLevel().addParticle(particle);
+        Main.getInstance().ftps.put(type, particle);
+    }
 
 }

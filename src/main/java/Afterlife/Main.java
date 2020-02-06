c3ebfe927a721ff363b033d834d2166cec2f2670
Steve Coulter <stevecoulter99@gmail.com>
2020-02-06 13:01:06 -0600

    register commands

--- src/main/java/Afterlife/Main.java
+++ src/main/java/Afterlife/Main.java
@@ -1,13 +1,19 @@
 package Afterlife;
 
+import Afterlife.commands.FloatingTextCommand;
+import Afterlife.commands.StatsCommand;
 import Afterlife.events.DamageEvent;
 import Afterlife.events.JoinEvent;
+import Afterlife.window.ProfileWindow;
+import PlayerAPI.Overrides.PlayerAPI;
+import cn.nukkit.command.CommandMap;
 import cn.nukkit.form.element.ElementInput;
 import cn.nukkit.form.window.FormWindow;
 import cn.nukkit.form.window.FormWindowCustom;
 import cn.nukkit.level.particle.FloatingTextParticle;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +29,9 @@ public class Main extends PluginBase {
 
     @Override
     public void onLoad() {
-
+        CommandMap commandMap = getServer().getCommandMap();
+        commandMap.register("Afterlife", new FloatingTextCommand("setleaderboard", this));
+        commandMap.register("Afterlife", new StatsCommand("stats", this));
     }
 
     @Override
@@ -53,4 +61,41 @@ public class Main extends PluginBase {
     public static Main getInstance() {
         return instance;
     }
+
+    public void sendProfile(PlayerAPI sender, String query) {
+
+        PlayerAPI player = (PlayerAPI) getServer().getPlayer(query);
+
+        try {
+            switch (getConfig().getString("view-stats")) {
+                case "standard":
+                    sender.sendMessage(TextFormat.GREEN+"= "+TextFormat.YELLOW+player.getName()+"'s leaderboard!"+TextFormat.GREEN+" =");
+                    sender.sendMessage(TextFormat.GREEN+"| "+TextFormat.GRAY+"Levels: "+TextFormat.WHITE+player.getLevels());
+                    sender.sendMessage(TextFormat.GREEN+"| "+TextFormat.GRAY+"Experience: "+TextFormat.WHITE+player.getXp());
+                    sender.sendMessage(TextFormat.GREEN+"| "+TextFormat.GRAY+"Most Kills: "+TextFormat.WHITE+player.getKills());
+                    sender.sendMessage(TextFormat.GREEN+"| "+TextFormat.GRAY+"Highest Kill-streak: "+TextFormat.WHITE+player.getKillStreak());
+                    sender.sendMessage(TextFormat.GREEN+"| "+TextFormat.GRAY+"Most Deaths: "+TextFormat.WHITE+player.getDeaths());
+                    sender.sendMessage(TextFormat.GREEN+"= "+TextFormat.YELLOW+"Usage: /stats <name>"+TextFormat.GREEN+" =");
+                    break;
+                case "form":
+                    String title = player.getName()+"'s Leaderboard";
+                    String content = "Levels: "+player.getLevels()+"\n"+
+                                    "Experience: "+player.getX()+"\n"+
+                                    "Most Kills: "+player.getKills()+"\n"+
+                                    "Highest Kill-streak: "+player.getKillStreak()+"\n"+
+                                    "Most Deaths: "+player.getDeaths()+"\n";
+                    ProfileWindow window = new ProfileWindow();
+                    window.setTitle(title);
+                    window.setContent(content);
+                    window.setButton1("search");
+                    window.setButton2("close");
+                    sender.showFormWindow(window);
+                    break;
+            }
+        } catch (NullPointerException e) {
+            sender.sendMessage(TextFormat.RED+"Player does not exist in our database!");
+        }
+
+    }
+
 }
--- src/main/java/Afterlife/commands/StatsCommand.java
+++ src/main/java/Afterlife/commands/StatsCommand.java
@@ -1,54 +1,34 @@
 package Afterlife.commands;
 
+import PlayerAPI.Overrides.PlayerAPI;
+import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.PluginCommand;
 import Afterlife.Main;
 
-import java.util.Map;
-
 public class StatsCommand extends PluginCommand {
 
-//    private Main plugin;
+    private Main plugin;
 
     public StatsCommand(String name, Main plugin) {
         super(name, plugin);
-//        this.plugin = plugin;
+        this.plugin = plugin;
+        this.setAliases(new String[]{"profile"});
     }
-//
-//    @Override
-//    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-//        if (sender instanceof PlayerAPI) {
-//
-//            switch (args.length) {
-//                case 0:
-//                    plugin.sendProfile((PlayerAPI) sender, sender.getName());
-//                    break;
-//                case 1:
-//                    Player player = this.getPlugin().getServer().getPlayer(args[0]);
-//                    if (player != null) {
-//                        plugin.sendProfile((Player) sender, player.getName());
-//                    } else {
-//                        try {
-//                            Map<String, Object> aliasDataMap = AliasPro.AliasPro.getPlayer(args[0]);
-//                            if (aliasDataMap == null) {
-//                                plugin.sendProfile((Player) sender, args[0]);
-//                            } else {
-//                                String uuid = aliasDataMap.get("uuid").toString();
-//                                Map<String, Object> objectMap = Mongodb.query(uuid, "uuid");
-//                                if (objectMap == null) {
-//                                    plugin.sendProfile((Player) sender, args[0]);
-//                                } else {
-//                                    plugin.sendProfile((Player) sender, objectMap.get("name").toString());
-//                                }
-//                            }
-//                        } catch (NoClassDefFoundError error) {
-//                            plugin.sendProfile((Player) sender, args[0]);
-//                        }
-//                    }
-//                    break;
-//            }
-//        } else {
-//            getPlugin().getLogger().error("command can only be run in-game");
-//        }
-//        return true;
-//    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        if (sender instanceof PlayerAPI) {
+            switch (args.length) {
+                case 0:
+                    plugin.sendProfile((PlayerAPI) sender, sender.getName());
+                    break;
+                case 1:
+                    // TODO - view other players data
+                    break;
+            }
+        } else {
+            getPlugin().getLogger().error("command can only be run in-game");
+        }
+        return true;
+    }
 }

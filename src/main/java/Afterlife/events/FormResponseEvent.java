56ba776c45749fa013d6dde935832b553c808ce9
Steve Coulter <stevecoulter99@gmail.com>
2020-02-17 19:21:39 -0600

    search player profile

--- src/main/java/Afterlife/events/FormResponseEvent.java
+++ src/main/java/Afterlife/events/FormResponseEvent.java
@@ -1,9 +1,11 @@
 package Afterlife.events;
 
 import Afterlife.Main;
+import PlayerAPI.Overrides.PlayerAPI;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerFormRespondedEvent;
+import cn.nukkit.form.response.FormResponseCustom;
 import cn.nukkit.form.response.FormResponseModal;
 
 public class FormResponseEvent implements Listener {
@@ -24,8 +26,19 @@ public class FormResponseEvent implements Listener {
             if (event.getFormID() == plugin.formAPI.getId("profile")) {
 
                 if (response.getClickedButtonId() == 0) {
-                    event.getPlayer().showFormWindow(plugin.formAPI.get("searchWindow"));
+                    event.getPlayer().showFormWindow(plugin.formAPI.get("searchWindow"), plugin.formAPI.getId("searchWindow"));
                 }
+
+            }
+
+        } else if (event.getResponse() instanceof FormResponseCustom) {
+
+            FormResponseCustom response = (FormResponseCustom) event.getResponse();
+
+            if (event.getFormID() == plugin.formAPI.getId("searchWindow")) {
+
+                String inputText = response.getInputResponse(0);
+                Main.getInstance().sendProfile((PlayerAPI) event.getPlayer(), inputText);
 
             }
 
